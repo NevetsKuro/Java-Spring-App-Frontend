85f0a138f1737559937a3aa952b39999e2fb8c96
steven <steven@Stark>
2017-12-21 21:25:07 +0530

    ProjectSave1
    controller done.
    dispatcher done.
    1 jsp Page

---
+++ src/main/java/com/niit/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.niit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/Home")
+	public String homepage()
+	{
+		return "welcomePage";
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<jsp:forward page="Home" />  
+</body>
+</html>
